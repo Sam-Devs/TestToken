e3b5cbbfff74883b9dde7bc112c1ad08682a5a49
Sam Ayorinde <41350405+Sam-Devs@users.noreply.github.com>
2021-07-01 13:25:45 +0100

    task added

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+require("@nomiclabs/hardhat-waffle");
+task("Balance", "Print account's balance")
+.addParam("account", "The account's address")
+.setAction(async taskArgs=> {
+  const account = await web3.utils.toChecksumAddress(taskArgs.account);
+  const balance = await web3.eth.getbalance(account);
+  console.log(web3.utils.fromWei(balance, 'ether'), "ETH");
+})
+
 module.exports = {
   solidity: "0.7.3",
 };
